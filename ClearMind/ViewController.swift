669e1892f2ee9725d1678fad2deeb93a6a344316
Asher Noel <asher13a@gmail.com>
2019-12-02 20:46:14 -0500

    Initial Commit

---
+++ ClearMind/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  ClearMind
+//
+//  Created by Asher Noel on 12/2/19.
+//  Copyright © 2019 Asher Noel. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
